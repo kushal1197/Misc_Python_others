c1734a3bc4af53994afe4cc8ca7c6c31c2db68d8
Prateek Mishra <mishra.eric@gmail.com>
2020-01-09 15:02:46 +0100

    lagrange and nrm

---
+++ lagrangeMultipliers.py
@@ -0,0 +1,50 @@
+"""
+Lagrange multipliers
+"""
+
+# Import libraries
+import numpy as np
+from scipy import optimize
+
+# First we define the functions, YOU SHOULD IMPLEMENT THESE
+def f (x, y) :
+    return -np.exp(x-y*y+x*y)
+
+def g (x, y) :
+    return np.cosh(y)+x-2
+
+# Next their derivatives, YOU SHOULD IMPLEMENT THESE
+def dfdx (x, y) :
+    return (-np.exp(x-y*y+x*y))*(1+y)
+
+def dfdy (x, y) :
+    return f(x,y)*(-2*y+x)
+
+def dgdx (x, y) :
+    return 1
+
+def dgdy (x, y) :
+    return np.sinh(y)
+
+# Use the definition of DL from previously.
+def DL (xyλ) :
+    [x, y, λ] = xyλ
+    return np.array([
+            dfdx(x, y) - λ * dgdx(x, y),
+            dfdy(x, y) - λ * dgdy(x, y),
+            - g(x, y)
+        ])
+
+# To score on this question, the code above should set
+# the variables x, y, λ, to the values which solve the
+# Langrange multiplier problem.
+
+# I.e. use the optimize.root method, as you did previously.
+
+x, y, λ = (0,0,0)
+
+print("x = %g" % x)
+print("y = %g" % y)
+print("λ = %g" % λ)
+print("f(x, y) = %g" % f(x, y))
+
